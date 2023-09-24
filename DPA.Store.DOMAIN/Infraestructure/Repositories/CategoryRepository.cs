490d609f368d5627df3b697c322950fd59754af9
Daniel <22102058@ue.edu.pe>
2023-09-23 21:30:45 -0500

    Agregar buscar por ID, eliminar y insert.

--- DPA.Store.DOMAIN/Infraestructure/Repositories/CategoryRepository.cs
+++ DPA.Store.DOMAIN/Infraestructure/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using DPA.Store.DOMAIN.Core.Entities;
+﻿using Azure.Core;
+using DPA.Store.DOMAIN.Core.Entities;
 using DPA.Store.DOMAIN.Core.Interfaces;
 using DPA.Store.DOMAIN.Infraestructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -22,5 +23,29 @@ namespace DPA.Store.DOMAIN.Infraestructure.Repositories
         {
             return await _dbContext.Category.ToListAsync();
         }
+
+        public async Task<Category> GetById(int id)
+        {
+            return await _dbContext.Category.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> Insert(Category category)
+        {
+            await _dbContext.Category.AddAsync(category);
+            var rows = await _dbContext.SaveChangesAsync();
+            return rows > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var category = await _dbContext.Category.Where(x =>x.Id == id).FirstOrDefaultAsync();
+            if(category==null)
+                return false;
+
+            _dbContext.Category.Remove(category);
+            var rows = await _dbContext.SaveChangesAsync();
+            return rows > 0;
+        }
+
     }
 }
